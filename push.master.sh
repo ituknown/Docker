8a24ba83b2bd800c76870a1cfae0c90de3e2ddbd
MinGRn <MinGRn97@gmail.com>
2018-12-11 18:27:04 +0800

    修改脚本

--- push.master.sh
+++ push.master.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+
+# git commits code both github and gitlab remote script, and branch is master!
+# if you want to commits to other branch, you need edit branch is <other> or 
+# add a new script!
+
+# Note: if current user does not have permission to execute the script, you need
+# execute the following command to give the user permission:
+# the command is: `chmod u+x push.master.sh`
+
+#-------------------------------------------------------------------------------
+# git push code to github remote and if you repository need have `github` remote
 git push github master
+
+#-------------------------------------------------------------------------------
+# git push code to gitlab remote and if you repository need have `github` remote
 git push gitlab master
